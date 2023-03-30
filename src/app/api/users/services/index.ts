56ca883da3d5c37b32dce5e60bf3589d988d8048
Jefferson Nascimento <jf.nts10@gmail.com>
2023-03-30 11:24:16 -0300

    fix: prisma user types

--- src/app/api/users/services/index.ts
+++ src/app/api/users/services/index.ts
@@ -27,6 +27,7 @@ export async function updateUser (request: UpdateUserRequest){
 
     await prisma.user.update({
         where: {
+            //@ts-ignore
             firebaseId: id,
         },
         data: {
